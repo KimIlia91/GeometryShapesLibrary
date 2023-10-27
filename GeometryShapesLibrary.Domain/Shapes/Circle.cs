c9ee956de5ddfda3568d957b8689aba1ece28f1b
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-27 17:21:19 +0600

    Added radius value object and circle class

---
+++ GeometryShapesLibrary.Domain/Shapes/Circle.cs
@@ -0,0 +1,48 @@
+﻿using GeometryShapesLibrary.Domain.Shapes.Circle.ValueObjects;
+
+namespace GeometryShapesLibrary.Domain.Shapes;
+
+/// <summary>
+/// Class for circle.
+/// </summary>
+public class Circle : Shape
+{
+    /// <summary>
+    /// Radius of ​​the circle.
+    /// </summary>
+    public Radius Radius { get; }
+
+    /// <summary>
+    /// Circle class constructor.
+    /// </summary>
+    /// <param name="radius">Radius of ​​the circle.</param>
+    /// <param name="area">Area of ​​the circle.</param>
+    private Circle(
+        Radius radius,
+        double area) : base(area)
+    {
+        Radius = radius;
+    }
+
+    /// <summary>
+    /// Create circle.
+    /// </summary>
+    /// <param name="radius">Radius of ​​the circle.</param>
+    /// <returns>Returns new circle.</returns>
+    public static Circle Create(double radius)
+    {
+        var radiusValueObject = Radius.Create(radius);
+        var area = GetArea(radiusValueObject);
+        return new Circle(radiusValueObject, area);
+    }
+
+    /// <summary>
+    /// Private method for calculate area of the circle.
+    /// </summary>
+    /// <param name="radius">Radius of ​​the circle.</param>
+    /// <returns>Area of the circle.</returns>
+    private static double GetArea(Radius radius)
+    {
+        return Math.PI * Math.Pow(radius.Value, 2);
+    }
+}
---
+++ GeometryShapesLibrary.Domain/Shapes/ValueObjects/Radius.cs
@@ -0,0 +1,47 @@
+﻿using GeometryShapesLibrary.Domain.Common.Models;
+using GeometryShapesLibrary.Domain.Common.Resources;
+
+namespace GeometryShapesLibrary.Domain.Shapes.ValueObjects;
+
+/// <summary>
+/// Class for radius.
+/// </summary>
+public class Radius : ValueObject
+{
+    /// <summary>
+    /// Value of the radius.
+    /// </summary>
+    public double Value { get; }
+
+    /// <summary>
+    /// Constructor of the radius class.
+    /// </summary>
+    /// <param name="value">Value of the radius.</param>
+    private Radius(double value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Create a radius.
+    /// </summary>
+    /// <param name="value">The value of the radius.</param>
+    /// <returns>A new instance of the Radius class with the specified value.</returns>
+    /// <exception cref="ArgumentException">Thrown when the provided value is less than or equal to zero.</exception>
+    public static Radius Create(double value)
+    {
+        if (value <= 0)
+            throw new ArgumentException(ErrorResources.NegativeNumber, nameof(value));
+
+        return new Radius(value);
+    }
+
+    /// <summary>
+    /// Gets the equality components of the radius.
+    /// </summary>
+    /// <returns>An IEnumerable containing the radius value.</returns>
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
