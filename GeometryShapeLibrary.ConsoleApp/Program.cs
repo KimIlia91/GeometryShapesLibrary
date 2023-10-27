eec036c2d7aae5445be3a01b58a77a3c6685b4be
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-27 18:07:51 +0600

    Fixed

---
+++ GeometryShapeLibrary.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using GeometryShapesLibrary.Domain.Shapes;
+
+var circle = Circle.Create(5);
+
+Console.WriteLine(circle.ToString());
+
+var triangle = Triangle.Create(5, 5, 5);
+
+Console.WriteLine(triangle.ToString());
--- GeometryShapesLibrary.ConsoleApp/GeometryShapesLibrary.ConsoleApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using GeometryShapesLibrary.Domain.Shapes;
-
-var circle = Circle.Create(5);
-Console.WriteLine(circle.ToString());
-
-var triangle = Triangle.Create(5, 5, 5);
-Console.WriteLine(triangle.ToString());
