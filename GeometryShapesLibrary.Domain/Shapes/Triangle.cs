1a00f45a4b7ed7638d902c60f8e074c9b9bd7bcd
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-27 18:03:10 +0600

    Added commnets

--- GeometryShapesLibrary.Domain/Shapes/Triangle.cs
+++ GeometryShapesLibrary.Domain/Shapes/Triangle.cs
@@ -2,10 +2,21 @@
 
 namespace GeometryShapesLibrary.Domain.Shapes;
 
+/// <summary>
+/// Represents a triangle shape defined by its sides.
+/// </summary>
 public class Triangle : Shape
 {
+    /// <summary>
+    /// Gets the sides of the triangle.
+    /// </summary>
     public Sides Sides { get; }
 
+    /// <summary>
+    /// Constructor to create a Triangle.
+    /// </summary>
+    /// <param name="sides">The sides of the triangle.</param>
+    /// <param name="area">The area of the triangle.</param>
     public Triangle(
         Sides sides,
         double area) : base(area)
@@ -13,9 +24,16 @@ public class Triangle : Shape
         Sides = sides;
     }
 
+    /// <summary>
+    /// Create a Triangle with the given side lengths.
+    /// </summary>
+    /// <param name="sideA">The length of the first side (Side A).</param>
+    /// <param name="sideB">The length of the second side (Side B).</param>
+    /// <param name="sideC">The length of the third side (Side C).</param>
+    /// <returns>A new instance of the Triangle class defined by its sides.</returns>
     public static Triangle Create(
-        double sideA, 
-        double sideB, 
+        double sideA,
+        double sideB,
         double sideC)
     {
         var sides = Sides.Create(sideA, sideB, sideC);
@@ -23,6 +41,11 @@ public class Triangle : Shape
         return new Triangle(sides, area);
     }
 
+    /// <summary>
+    /// Calculate the area of the triangle using its sides.
+    /// </summary>
+    /// <param name="sides">The sides of the triangle.</param>
+    /// <returns>The calculated area of the triangle.</returns>
     private static double GetArea(Sides sides)
     {
         double s = (sides.SideA + sides.SideB + sides.SideC) / 2;
