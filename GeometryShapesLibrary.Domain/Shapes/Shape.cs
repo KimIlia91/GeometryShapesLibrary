08f0dc401362c5bf33163272d265d98cb9d31be7
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-27 17:07:53 +0600

    Added shape description class

--- GeometryShapesLibrary.Domain/Shapes/Shape.cs
+++ GeometryShapesLibrary.Domain/Shapes/Shape.cs
@@ -1,15 +1,29 @@
 ﻿namespace GeometryShapesLibrary.Domain.Shapes;
 
+/// <summary>
+/// Base class for geometric shapes.
+/// </summary>
 public abstract class Shape
 {
+    /// <summary>
+    /// Area of ​​the shape.
+    /// </summary>
     public double Area { get; }
 
+    /// <summary>
+    /// Shape class constructor.
+    /// </summary>
+    /// <param name="area">Area of ​​the shape.</param>
     public Shape(
         double area)
     {
         Area = area;
     }
 
+    /// <summary>
+    /// Returns a string representation of the shape.
+    /// </summary>
+    /// <returns>Returns a string representation of the shape</returns>
     public override string ToString()
     {
         return $"Type: {GetType().Name}, Area: {Area}";
