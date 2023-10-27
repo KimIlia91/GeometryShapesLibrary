861591911aa579e7e7af8be44198592db44a6705
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-27 17:06:07 +0600

    Added shape base class

---
+++ GeometryShapesLibrary.Domain/Shapes/Shape.cs
@@ -0,0 +1,17 @@
+﻿namespace GeometryShapesLibrary.Domain.Shapes;
+
+public abstract class Shape
+{
+    public double Area { get; }
+
+    public Shape(
+        double area)
+    {
+        Area = area;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: {GetType().Name}, Area: {Area}";
+    }
+}
